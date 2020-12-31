6b1775f10d85854626cbdff117fdb080b8400163
Kelly Shearer <kellyshearer11@gmail.com>
2020-12-31 00:02:59 +0000

    navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import { Header } from './components/header';
+import { Footer } from './components/footer';
+import { Content } from './components/content';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Nav } from 'react-bootstrap';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-function App() {
+
+class App extends Component {
+  render() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="dark" variant="dark">
+      <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+      <Nav className="mr-auto">
+        <Nav.Link href="/">Home</Nav.Link>
+        <Nav.Link href="/read">Read</Nav.Link>
+        <Nav.Link href="/create">Create</Nav.Link>
+      </Nav>
+    </Navbar>
+
+    <Switch>
+      <Route path='/' component={Content} exact />
+      <Route exact path='/create' component={Footer} />
+      <Route path='/read' component={Header} />
+    </Switch>
     </div>
+    </Router>
   );
-}
+  }
+  }
+  
 
 export default App;
---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export class Header extends React.Component {
+
+    render(){
+        return(
+            <div>
+                <h1> header component </h1>
+            </div>
+        );
+    }
+}
